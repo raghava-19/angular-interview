3cca69b3dff4e2f77f5cbfe322d9aafd57d77ee8
nukala raghava <nukalaraghava.01@gmail.com>
2022-09-02 10:28:38 +0530

    added template to display options provided

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Option } from './components/multi-check.component';
-
+import  * as MultiCheckLabels from '../assets/multi-check-labels.json';
 const defaultValues: string[] = [];
+export type MultiCheckInput ={
+  options : Option[];
+  values: string[];
+  labels: Object;
+  columns: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-  options: Option[] = [
+export class AppComponent implements OnInit{
+  public jsondata  = MultiCheckLabels;
+  public options: Option[] = [
     { label: 'aaa', value: '111' },
     { label: 'bbb', value: '222' },
     { label: 'ccc', value: '333' },
@@ -20,10 +27,34 @@ export class AppComponent {
     { label: 'hhh', value: '888' },
     { label: 'iii', value: '999' },
   ];
+  public selectedValues:string[] = defaultValues;
+  public selectAllOption!: Option;
+  public multiCheckInput!: MultiCheckInput;
 
-  selectedValues:string[] = defaultValues;
+  constructor() {
+    this.selectAllOption = {
+      label: this.jsondata.select_all_label,
+      value: this.jsondata.select_all_value
+    }
+  }
 
-  onSelectedOptionsChange(options: Option[]): void {
+  ngOnInit(): void {
+    this.options?.length > 0 ? this.options?.unshift(this.selectAllOption) :'';
+    this.frameOptionsOnLoad();
+  }
+
+  public onSelectedOptionsChange(options: Option[]): void {
     this.selectedValues = options.map((it) => it.value);
+  }
+  /**
+   * framing an input with all required data to pass to multi-check component
+   */
+  private frameOptionsOnLoad(){
+    this.multiCheckInput = {
+      options: this.options,
+      values: this.selectedValues,
+      labels: this.jsondata,
+      columns: this.jsondata.columns_shown
+    }
   }
 }
--- src/app/components/multi-check.component.ts
+++ src/app/components/multi-check.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChange, SimpleChanges } from '@angular/core';
+import { MultiCheckInput } from '../app.component';
 
 export type Option = {
 	label: string;
@@ -21,8 +22,30 @@ export type Option = {
 @Component({
   selector: 'app-multi-check',
   templateUrl: './multi-check.component.html',
-  styleUrls: ['./multi-check.component.less']
+  styleUrls: ['./multi-check.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MultiCheckComponent {
-  // @todo
+export class MultiCheckComponent implements OnChanges ,AfterViewInit {
+
+  @Input() multiCheckInput = {} as MultiCheckInput;
+  @Output() onChange = new EventEmitter<Option[]>();
+  public label!: string;
+  public columns!: number;
+  public options : Option[] = []
+
+
+  ngOnChanges(changes: SimpleChanges) {
+   if(changes.multiCheckInput){
+      this.options = changes.multiCheckInput.currentValue.options;
+      this.label = changes.multiCheckInput.currentValue.labels.label; 
+      this.columns = changes.multiCheckInput.currentValue.labels.columns_shown;
+    }
+  }
+
+
+  ngAfterViewInit() {
+    const item = document.getElementById('options_container');
+    item!.style.columnCount = String(this.columns);
+  }
+
 }
